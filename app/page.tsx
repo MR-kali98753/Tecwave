2df80544ce1094dbe1cb6f10394deed2a83bf6ee
MR kali <mrkali98753@gmail.com>
2025-05-25 21:01:48 +0530

    added missing Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Ads from "@/components/Ads";
 import Blogs from "@/components/Blogs";
+import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Header />
       <Ads />
       <Blogs />
     </>
